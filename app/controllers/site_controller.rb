7f9aca27e3536c4463e4320d1c0d869050214ed9
bksanda <YOUR-EMAIL-ADDRESS>
2015-01-24 10:05:59 -0800

    user_controller methods

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,6 +1,10 @@
 class SiteController < ApplicationController
 
   def index
+    if @current_user
+      redirect_to user_path
+    end
   end
 
-end
+  def about
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to login_path
-    else
+    if @user.errors.any?
       render 'new'
+    else
+      flash[:success] = 'User has been created'
+      redirect_to login_path
+    end
   end
 
   def new
@@ -18,23 +20,41 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find_by_id(params[:id])
+    @goal = Goal.find_by_id(params[:goal_id])
     redirect_to user_path
   end
 
   def update
+    @user = User.find_by_id(params[:id])
+
+    @user.goals.clear
+    @goals = params[:user][:goal_id]
+
+    @goals.each do |goal_id|
+      @user.goals << Goal.find(goal_id) unless goal_id.blank?
+    end
+
+    redirect_to user_path
   end
 
   def destroy
   end
 
-  def goals
+  def goal
+    @goal = Goal.find_by_id(params[:id])
+    @users = goal ? goal.users : []
   end
 
   def goals_update
   end
 
+  def awards
+  end
+
 
   private
+
   def user_params
     params.require(:user).permit(:name,:email,:password)
   end
