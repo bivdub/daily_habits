1ae2105c35225d4fa4097cf1743938e4b1dae9df
bksanda <YOUR-EMAIL-ADDRESS>
2015-01-26 07:47:19 -0800

    adds to controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,9 @@ Rails.application.routes.draw do
   get "about" => "site#about"
 
   resources :users
-  get 'signup' => 'users#new', as: :signup
+
+  get 'signup' => 'site#new', as: :signup
+  post 'signup' => 'site#create'
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
@@ -31,50 +33,4 @@ Rails.application.routes.draw do
   get "users/:id/awards" => "users#awards"
 
 
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
