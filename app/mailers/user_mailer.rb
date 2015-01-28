a95d8cb8241cafde162032e318ec31e06f7b21a2
bksanda <YOUR-EMAIL-ADDRESS>
2015-01-27 23:51:40 -0800

    sendgrid setup and test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,13 @@
 class UserMailer < ApplicationMailer
+  default :from => "sanda.htyte@gmail.com"
+
+  def registration_confirmation(user)
+    @user = user
+    mail(:to => "#{user.name}<#{user.email}>", :subject => "Registered") #:from => "sanda.htyte@gmail.com")
+  end
+
+  def email_notify(user)
+    @user = user
+    mail(:to => "#{user.name}<#{user.email}>", :subject => "Email notifications set") #:from => "sanda.htyte@gmail.com")
+  end
 end
